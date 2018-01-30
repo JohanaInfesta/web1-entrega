59c6c1f3866ec90a0f21b168132cd36878a434d7
Johana Infesta <johana.infesta@gmail.com>
2018-01-30 16:02:19 -0300

    trabajando

--- entrega_web1/js/script.js
+++ entrega_web1/js/script.js
@@ -19,6 +19,9 @@ $(document).ready(function () {
       type: 'GET',
       success: function(result){
         $(".main-content ").html(result);
+        if () {
+
+        }
       }
     })
   })
@@ -33,50 +36,54 @@ function renderList(){
 
     $('.contenido-comentario contenido-comentario').html('');
 
-    list.sort(function(a, b){
-      return a.position - b.position;
-    });
-
-    list.forEach(function(ranking){
-      renderRanking(ranking);
+    list.forEach(function(foro){
+      renderRanking(foro);
     });
   });
-}
+}*/
+  /*
+  function getRankingList() {
+    return new Promise(function (resolve, reject) {
+        let list = [];
+        $.get(servicioRest + 'group/' + groupId, function (data) {
+            data.information.forEach(function (i) {
 
-function getRankingList(callback){
-  let list = [];
-  $.get(servicioRest + 'group/' + groupId, function (data) {
-    data.information.forEach(function (i) {
+                let thing = {};
 
-      let thing = {};
+                if (typeof i['thing'] === 'object')
+                    thing = i['thing'];
 
-      if (typeof i['thing'] === 'object')
-      thing = i['thing'];
-
-      thing.id = i['_id'];
-      list.push(thing);
+                thing.id = i['_id'];
+                list.push(thing);
+            });
+            resolve(list);
+        });
     });
-    resolve(list);
-  });
 }
+*/
 
+/*
 function iniciarForo(){
   let list =[{
-    nombre : 'asd',
-    comentario : 'asdfghj qwertyu zxcvbn',
+    nombre : 'Legend',
+    comentario : 'Todo parece ser como lo es la empresa animadora de la serie Code Geass filtró un vídeo avance de la supuesta tercera temporada de esta famosa serie, el cual ha causado mucho revuelo puesto que prestigiosas paginas parecen avalar esta noticia, el titulo por la cual pretende regresar esta icónica serie es (Lelouch of the Resurrection) aun por lo pronto nada esta oficializado dado que la empresa Sunrise no ha anunciado el regreso de la tercera temporada aunque tampoco lo ha negado, entre las tantas posibilidades existe la probabilidad de que este sea un vídeo recopila-torio como la mayoría de las películas relacionadas a esta serie.',
   },{
-    nombre :'qwerty',
-    comentario : 'werty asdf pojvzxcv',
+    nombre :'Yuna',
+    comentario : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
+  },{
+    nombre :'Yuna',
+    comentario : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
   }];
 
-  cleanList().then(function (results) {
-    let asdf = [];
-
-    for (let i = 0; i < list.length; i++) {
-      asdf.push(createRanking(list[i]));
-    }
-
-    callback.all(asdf).then(renderList);
-  });
 }
+*/
+/*cleanList().then(function (results) {
+let asdf = [];
+
+for (let i = 0; i < list.length; i++) {
+asdf.push(createRanking(list[i]));
+}
+
+callback.all(asdf).then(renderList);
+});
 */
