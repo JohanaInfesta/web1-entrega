9fedd4e61e47e638c26c609231709295b2a7d0ef
Johana Infesta <johana.infesta@gmail.com>
2018-01-31 17:07:12 -0300

    ajax, api rest, js, vayanse bien a la mierda... no me sale una bosta

--- entrega_web1/js/script.js
+++ entrega_web1/js/script.js
@@ -17,10 +17,8 @@ $(document).ready(function () {
     $.ajax({
       url: $(this).attr('href'),
       type: 'GET',
-      success: function(result){
-        leerForo();
+      success:function(result){
         $(".main-content").html(result);
-
       }
     })
   })
@@ -28,112 +26,80 @@ $(document).ready(function () {
 
 /*
 function iniciarForo(){
+}
+*/
 
+let groupId = 1135;
+let servicioRest = 'https://web-unicen.herokuapp.com/api/thing';
 
-}*/
-let groupId = 135;
-let servicioRest = 'https://web-unicen.herokuapp.com/api/thing/';
+function probando(){
 
-function crearForo(Foro) {
-
-  let info = {
+  event.preventDefault();
+  let info ={
     group: groupId,
-    thing: ranking,
+    thing: servicioRest,
   };
+
   $.ajax({
-    method: "POST",
-    dataType: 'JSON',
-    data: JSON.stringify(info),
-    contentType: "application/json; charset=utf-8",
-    url: servicioRest,
-    success: function (resultData) {
-      leerForo();
+    method: "GET",
+    dataType:'JSON',
+    url: "https://web-unicen.herokuapp.com/api/thing/group/1135",
+    success: function(resultData){
+     $(".probando").html(html);
     },
     error: function (jqxml, status, errorThrown) {
-
+      console.log(errorThrown);
+      alert ("asdfg");
     }
   });
+
 }
 
-function leerForo (){
 
-  let list =[{
-    nombre : 'Legend',
-    comentario : 'Todo parece ser como lo es la empresa animadora de la serie Code Geass filtró un vídeo avance de la supuesta tercera temporada de esta famosa serie, el cual ha causado mucho revuelo puesto que prestigiosas paginas parecen avalar esta noticia, el titulo por la cual pretende regresar esta icónica serie es (Lelouch of the Resurrection) aun por lo pronto nada esta oficializado dado que la empresa Sunrise no ha anunciado el regreso de la tercera temporada aunque tampoco lo ha negado, entre las tantas posibilidades existe la probabilidad de que este sea un vídeo recopila-torio como la mayoría de las películas relacionadas a esta serie.',
-  },{
-    nombre :'Yuna',
-    comentario : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
-  },{
-    nombre :'Tidus',
-    comentario : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
-  }];
+/* compruebo que los campos esten completos para continuar, sino tiro un alert y pido todos los campos
+function agregarComentario(){
+$('.enviar').attr('disable', true);
+
+let contenido = {
+nombre : $('.js-input-nombre').val(),
+comentario : $('.js-input-comentario').val();
+}
+let completo = true;
+
+for (let campo in completo) {
+if (!contenido[campo]) {
+alert("Completar todos los campos");
+completo = false;
+break;
+}
+}
+if (completo) {
+$('.js-input-nombre').val('');
+$('.js-input-comentario').val('');
+subirComemtario(contenido, function(){
+leerForo();
+$('.enviar').attr('disabled', false);
+});
+else {
+$('enviar').attr('disabled', false);
+}
+}
 }
 
+function leerForo(){
+$('.tabla-topten tbody').html('<div class="loading"><i class="fa fa-cog fa-spin fa-3x fa-fw"></i></div>');
+}
+*/
 /*
-cleanList().then(function(results){
-let coments = [];
-for (let i = 0; i < list.length; i++) {
-coments.push(createRanking(list[i]));
-}
-return this(coments).then(renderList);
-})
-/*
-let myNombre = document.createElement('.js-foro-nombre');
-myNombre.textContent = list['nombre'];
-header.appendChild(myNombre);
-
-console.log(myNombre.textContent);
-
-let myComentario = document.createElement('.js-foro-comentario');
-myComentario.textContent = list['comentario'];
-header.appendChild(myComentario); */
-}
-
-
-/*comentarios
-function renderList(){
-$('.contenido-comentario contenido-comentario').html('<div class="loading"><i class="fa fa-cog fa-spin fa-3x fa-fw"></i></div>');
-getRankingList().then(function(list){
-
-$('.contenido-comentario contenido-comentario').html('');
-
-list.forEach(function(foro){
-renderRanking(foro);
-});
-});
-}
-
-function getRankingList() {
-return new Promise(function (resolve, reject) {
-let list = [];
-$.get(servicioRest + 'group/' + groupId, function (data) {
-data.information.forEach(function (i) {
-
-let thing = {};
-
-if (typeof i['thing'] === 'object')
-thing = i['thing'];
-
-thing.id = i['_id'];
-list.push(thing);
-});
-resolve(list);
-});
-});
-}
-
-
-
-
-
-cleanList().then(function (results) {
-let promises = [];
-
-for (let i = 0; i < list.length; i++) {
-promises.push(createRanking(list[i]));
-}
-
-Promise.all(promises).then(renderList);
-});
+let list =[{
+nombre : 'Legend',
+comentario : 'Todo parece ser como lo es la empresa animadora de la serie Code Geass filtró un vídeo avance de la supuesta tercera temporada de esta famosa serie, el cual ha causado mucho revuelo puesto que prestigiosas paginas parecen avalar esta noticia, el titulo por la cual pretende regresar esta icónica serie es (Lelouch of the Resurrection) aun por lo pronto nada esta oficializado dado que la empresa Sunrise no ha anunciado el regreso de la tercera temporada aunque tampoco lo ha negado, entre las tantas posibilidades existe la probabilidad de que este sea un vídeo recopila-torio como la mayoría de las películas relacionadas a esta serie.',
+},{
+nombre :'Yuna',
+comentario : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
+},{
+nombre :'Tidus',
+comentario : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
+}];
 }
 */
