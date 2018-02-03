f56a4ef0941bb7e20f24be9157540001a2f1ec9d
Johana Infesta <johana.infesta@gmail.com>
2018-02-03 17:01:34 -0300

    anda el post y el get pero no logro traer el obj dentro del thing

--- entrega_web1/js/script.js
+++ entrega_web1/js/script.js
@@ -24,22 +24,13 @@ $(".navigate").on("click", function(event)
     }
   });
   $(".main-content").html("<h1>Loading...</h1>");
-  if ($('.main-content').find('.js-foro').length) {
-    getForo().then(function(list){
-      if (!list.length) {
-        iniciarForo();
-      } else {
-        leerComentario();
-      }
-    });
-  }
   /*meter un if para cargar el foro */
   event.preventDefault();
 });
 
 function resetComentarios(){
   event.preventDefault();
-  let grupo = 1135;
+  let grupo = 1136;
   $.ajax({
     method: "GET",
     dataType: 'JSON',
@@ -50,7 +41,8 @@ function resetComentarios(){
       for (let i = 0; i < resultData.information.length; i++) {
         html += "Id: " + resultData.information[i]['_id'] + "<br />";
         html += "Grupo: " + resultData.information[i]['group'] + "<br />";
-        html += "Informacion: " + resultData.information[i]['thing'] + "<br />";
+        html += "Informacion: " + resultData.information[i]['thing.nombre'] + "<br />";
+        html += "Informacion: " + resultData.information[i]['comentario'] + "<br />";
         html += "--------------------- <br />";
       }
       $("#infoGroup").html(html);
@@ -60,27 +52,44 @@ function resetComentarios(){
     }
   });
 }
-/*
-function iniciarForo(){
+
+function enviarComentario(dbody){
+  event.preventDefault();
+  let grupo= 1136;
+  let anda ={
+    "nombre":$(".js-input-nombre").val(),
+    "comentarios":$(".js-input-comentario").val(),
+  };
+  let info ={
+    group: grupo,
+    thing: anda,
+  };
+  if (grupo && anda){
+    $.ajax({
+      url:"http://web-unicen.herokuapp.com/api/thing",
+      type:'POST',
+      dataType: 'JSON',
+      data:JSON.stringify(info),
+      contentType: "application/json; charset=utf-8",
+      success:function(resultData){
+        $("#guardarAlert").html("Informacion guardada con ID =" + resultData.information._id);
+        console.log(resultData);
+      },
+      error:function(jqxml, status, errorThrown){
+        console.log(errorThrown);
+      }
+    });
+  }
+}
+
 
 let list =[{
-nombre : 'Legend',
-comentario : 'Todo parece ser como lo es la empresa animadora de la serie Code Geass filtró un vídeo avance de la supuesta tercera temporada de esta famosa serie, el cual ha causado mucho revuelo puesto que prestigiosas paginas parecen avalar esta noticia, el titulo por la cual pretende regresar esta icónica serie es (Lelouch of the Resurrection) aun por lo pronto nada esta oficializado dado que la empresa Sunrise no ha anunciado el regreso de la tercera temporada aunque tampoco lo ha negado, entre las tantas posibilidades existe la probabilidad de que este sea un vídeo recopila-torio como la mayoría de las películas relacionadas a esta serie.',
+  "nombre" : 'Legend',
+  "comentario" : 'Todo parece ser como lo es la empresa animadora de la serie Code Geass filtró un vídeo avance de la supuesta tercera temporada de esta famosa serie, el cual ha causado mucho revuelo puesto que prestigiosas paginas parecen avalar esta noticia, el titulo por la cual pretende regresar esta icónica serie es (Lelouch of the Resurrection) aun por lo pronto nada esta oficializado dado que la empresa Sunrise no ha anunciado el regreso de la tercera temporada aunque tampoco lo ha negado, entre las tantas posibilidades existe la probabilidad de que este sea un vídeo recopila-torio como la mayoría de las películas relacionadas a esta serie.',
 },{
-nombre :'Yuna',
-comentario : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
+  "nombre" :'Yuna',
+  "comentario" : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
 },{
-nombre :'Tidus',
-comentario : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
+  "nombre" :'Tidus',
+  "comentario" : 'Con la publicación de tercer tomo del manga Vigilante (Boku No Hero Academia ILLEGALS) la portada del tomo a hizo mención sobre la fecha de lanzamiento de la tercera temporada del anime Boku No Hero Academia, la fecha prevista para el día de lanzamiento es el 7 de abril.',
 }];
-cleanList().then(function (results) {
-let promises = [];
-
-for (let i = 0; i < list.length; i++) {
-promises.push(crearComentario(list[i]));
-}
-
-Promise.all(promises).then(renderList);
-});
-}
-*/
