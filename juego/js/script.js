6459f7213c4b03768d35a1c60fcf55eb8df29d9c
Johana Infesta <johana.infesta@gmail.com>
2018-02-15 14:10:14 -0300

    pagina web y juego terminado

--- juego/js/script.js
+++ juego/js/script.js
@@ -27,6 +27,21 @@ $(document).ready(function(){
   $("#piedra, #papel, #tijera, #lagarto, #spock").on("click", function() {
     jugar();
   });
+  $("#js-spockfriendly").on("click", function() {
+    if ($("#js-spockfriendly").is(":checked")) {
+      alert("¡Modo Spock Friendly activado!");
+    }
+    else {
+      alert("¡Modo Spock Friendly desactivado!");
+    }
+  });
+  $("#js-best").on("click", function(){
+    if ($("#js-best").is(":checked")) {
+      alert("¡Modo Best Of 3 activado!");
+    }else {
+      alert("Modo Best Of 3 desactivado");
+    }
+  });
   let jugar = function() {
 
     let juegaCompu = function() {
@@ -36,6 +51,7 @@ $(document).ready(function(){
     juegaCompu();
 
     let generarJuego = function() {
+      creditos-=5;
       totalPartidas++;
       let opcionusuario = usuario;
       let opcioncompu = computadora;
@@ -48,6 +64,7 @@ $(document).ready(function(){
           if((computadora == 2) || (computadora == 3)){
             alert("ganaste");
             partidasGanadas++;
+            creditos+=10;
           }else{
             if(computadora== 0){
               alert("empate");
@@ -64,6 +81,7 @@ $(document).ready(function(){
           if((computadora == 0) || (computadora == 4)){
             alert("ganaste");
             partidasGanadas++;
+            creditos+=10;
           }else{
             if(computadora == 1){
               alert("empate");
@@ -76,6 +94,7 @@ $(document).ready(function(){
         if((computadora == 1) || (computadora == 3)){
           alert("ganaste");
           partidasGanadas++;
+          creditos+=10;
         }else{
           if((computadora== 0) || (computadora== 4)){
             alert("perdiste");
@@ -92,6 +111,7 @@ $(document).ready(function(){
         if((computadora == 1) || (computadora == 4)){computadora
           alert("ganaste");
           partidasGanadas++;
+          creditos+=10;
         }else{
           if((computadora == 0) || (computadora == 2)){
             alert("perdiste");
@@ -108,6 +128,7 @@ $(document).ready(function(){
         if((computadora == 0) || (computadora == 2)){
           alert("ganaste");
           partidasGanadas++;
+          creditos+=10;
         }else{
           if((computadora == 1) || (computadora == 3)){
             alert("perdiste");
@@ -128,29 +149,131 @@ $(document).ready(function(){
     generarJuego();
 
     let modoSpockFriendly = function() {
-      creditos-=5
       let opcionusuario = usuario;
       let opcioncompu = computadora;
       if ($("#js-spockfriendly").is(":checked")) {
-        if (empates) {
-        }else if (partidasPerdidas) {
-        }else {
-          if(opcionusuario== 4)
-          creditos +=20;
-          else
-          credito +=10;
+        if((opcionusuario == 4)&&(opcioncompu==2)||(opcionusuario==4)&&(opcioncompu==0)){
+          creditos+=5;
         }
       }
     }
     modoSpockFriendly();
 
-    let actualizarTabla = function(){
-      $('.creditos').html(creditos);
-      $('.total-juego').html(totalPartidas);
-      $('.total-usuario').html(partidasGanadas);
-      $('.total-computadora').html(partidasPerdidas);
-      $('.total-empates').html(empates)
+    let modoBestOf = function(){
+      if ($("#js-best").is(":checked")) {
+        creditos = 100;
+        let PartidasTotales = totalPartidas;
+        if (PartidasTotales===3) {
+
+          if (partidasGanadas >=2 ) {
+            alert("ganaste");
+            creditos +=5;
+          }
+          else if (partidasGanadas === partidasPerdidas) {
+            alert("empate");
+            creditos -=5
+          }
+          else {
+            alert("perdiste");
+            creditos -=5;
+          }
+        }
+      }
     }
-    actualizarTabla();
-  }
+    modoBestOf();
+
+    $('.creditos').html(creditos);
+
+    let subirTabla = function(){
+      event.preventDefault();
+      let grupo = 1139;
+      let tabla ={
+        "totales" : totalPartidas,
+        "ganadas" : partidasGanadas,
+        "perdidas" : partidasPerdidas,
+        "empate" : empates,
+      };
+      let info ={
+        group : grupo,
+        thing : tabla,
+      };
+      if (grupo && tabla) {
+        $.ajax({
+          url:"http://web-unicen.herokuapp.com/api/thing",
+          type:'POST',
+          dataType: 'JSON',
+          data:JSON.stringify(info),
+          contentType: "application/json; charset=utf-8",
+          success:function(resultData){
+            console.log(resultData);
+          },
+          error:function(jqxml, status, errorThrown){
+            console.log(errorThrown);
+          }
+        });
+      }else{
+        alert("error");
+      }
+    }
+    subirTabla();
+
+    let mostrarTabla = function(){
+      let grupo = 1139;
+      $.ajax({
+        method: "GET",
+        dataType: 'JSON',
+        url: "http://web-unicen.herokuapp.com/api/thing/group/" + grupo,
+        success: function (resultData){
+          let html =" ";
+          for (let i = 0; i < resultData.information.length; i++) {
+            html += "<tr id='"+ resultData.information[i]._id +"'>";
+            html += "<td >"+ resultData.information[i].thing['totales'] + "</td>";
+            html += "<td>" + resultData.information[i].thing['ganadas'] + "</td>";
+            html += "<td>" + resultData.information[i].thing['perdidas'] + "</td>";
+            html += "<td>" + resultData.information[i].thing['empate'] + "</td>";
+            html += "<td ><button class='glyphicon glyphicon-remove eliminarComentario' resultData-id='"+resultData.information[i]._id+"'></button></td></tr>";
+          }
+          $("#template").html(html);
+          console.log(resultData);
+
+          $(".eliminarComentario").on("click", function() {
+            event.preventDefault;
+            let _id = $(this).parent().parent().attr("id");// busca el id directo para borrar
+            $.ajax({
+              url : "http://web-unicen.herokuapp.com/api/thing/" + _id,
+              method : "DELETE",
+              contentType : "application/json; charset=utf-8",
+              dataType : "JSON",
+              success : function(resultData){
+                $("#template").html(html);
+                return (mostrarTabla());
+              },
+              error : function(xmlhr, r, error){
+                alert("Error. Intente más tarde");
+              }
+            });
+          });
+        }
+      });
+    }
+    mostrarTabla();
+    /*
+    let reset = function(){
+    $('#reset').on('click', function(){
+    e.preventDefault();
+    $.ajax({
+    url:"http://web-unicen.herokuapp.com/api/thing/group/1139" +_id,
+    type:'DELETE',
+    dataType: 'JSON',
+    data:JSON.stringify(id),
+    contentType: "application/json; charset=utf-8",
+    success: function (resultData){
+    console.log(resultData);
+  },
+});
+});
+console.log(reset);
+}
+reset();*/
+}
 })
